8db575e2784b15536cba0e5e3d262b45d06be2f9
Jazmín Del Puerto <jazmindelpuerto@gmail.com>
2023-12-28 16:29:24 -0300

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import "./Weather.css";
 export default function Weather() {
   return (
-    <div className="weather">
+    <div className="Weather">
       <form>
         <div className="row">
           <div className="col-8">
@@ -9,10 +9,11 @@ export default function Weather() {
               type={"search"}
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-4">
-            <input type={"submit"} value="Search" className="btn btn-primary" />
+            <input type={"submit"} value="Search" className="btn btn-light" />
           </div>
         </div>
       </form>
@@ -22,12 +23,17 @@ export default function Weather() {
         <li>Mostly cloudly</li>
       </ul>
       <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="weather icon"
-          />
-          33°C
+        <div className="col-6 ">
+          <span className="d-flex align-items-center">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="weather icon"
+              className="img-fluid"
+            />
+
+            <span className="temperature">33</span>
+            <span className="unit">°C</span>
+          </span>
         </div>
         <div className="col-6">
           <ul>
