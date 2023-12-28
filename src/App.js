fee106b0ea6deca932372279345aa4142afee138
Jazmín Del Puerto <jazmindelpuerto@gmail.com>
2023-12-28 14:26:37 -0300

    Added footer

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> Weather App </h1>
+      <footer>
+        This project was coded by Jazmin Del Puerto and is{" "}
+        <a
+          href="https://github.com/jazmindelp/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://react-weather-shecodes-project.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
+}
